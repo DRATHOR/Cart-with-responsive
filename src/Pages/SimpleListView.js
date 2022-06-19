f4746453a5e809ee78825416eb5551bfd58c0c5d
devendra <devendra143143@gmail.com>
2022-06-18 18:52:49 -1200

    add pluse icon in mobile veiw

--- src/Pages/SimpleListView.js
+++ src/Pages/SimpleListView.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { addIcon } from '../Assets/Icons'
 import ListCard from '../Components/ListCard/ListCard'
-import { allOrderList } from '../Jsons/allTypeOfOrder'
+import './SimpleListView.scss'
 
-const SimpleListView = ({list}) => {
+
+const SimpleListView = ({ list }) => {
+  const navigate=useNavigate();
   return (
     <div className='p-16'>
-        {list?.map(listItem=>{
-            return(
-                <ListCard listItem={listItem}/>
-            )
-        })}
+      {list?.map(listItem => {
+        return (
+          <ListCard listItem={listItem} />
+        )
+      })}
+      <div className='floating_add_button' >
+        <img src={addIcon} alt="" onClick={() => navigate('/order')} />
+      </div>
     </div>
   )
 }
